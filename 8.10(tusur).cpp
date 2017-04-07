76b8b25033fbd53e92afff12e75935440605e15e
Наташенька ❤️ <evilberry@icloud.com>
2017-04-07 19:12:37 +0400

    Add files via upload

---
+++ 8.10(tusur).cpp
@@ -0,0 +1,31 @@
+#include <QCoreApplication>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int array[5][5];
+    for(int i = 0;i < 5;i++){
+        for(int j = 0;j < 5;j++){
+            array[i][j] = 1 + rand() % 3;
+        }
+    }
+    //минимальное число
+    int sum = 0,min=100,who;
+    for(int i = 0;i < 5;i++){
+        for(int j = 0;j < 5;j++){
+            sum = sum + array[i][j];
+        }
+        if(sum < min){
+            min = sum;
+            who = i;
+        }
+        sum = 0;
+    }
+    while (cout <= 15){
+
+        cout++;
+    }
+    cout << "Winner - " << who << endl;
+    //system("pause");
+    return 0;
+}
